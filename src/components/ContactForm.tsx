bf35e4d0592828502cf742c83a223472f2ea538b
rajkorde <rajkorde68@gmail.com>
2025-08-14 22:40:57 +0530

    Update ContactForm.tsx

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -86,10 +86,8 @@ const ContactForm = () => {
                   </div>
                   <div>
                     <p className="font-medium">Call Us</p>
-                    <a href="tel:9104629229" className="text-muted-foreground hover: text-primary transition-colors">  9104629229 </a>
-                     <a href="tel:9104629229" className="text-muted-foreground hover: text-primary transition-colors">  9998491595 </a>
-                    <p className="text-muted-foreground">9104629229</p>
-                    <p className="text-muted-foreground">9998491595</p>
+                    <a href="tel:9104629229" className="text-muted-foreground hover: text-primary transition-colors">  9104629229 </a> </ br>
+                    <a href="tel:9104629229" className="text-muted-foreground hover: text-primary transition-colors">  9998491595 </a>
                   </div>
                 </div>
                 <div className="flex items-center gap-4">
