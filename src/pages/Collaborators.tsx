e5da51b39afd75a4926a79d4b0ce8204b4524525
rajkorde <rajkorde68@gmail.com>
2025-08-14 22:54:57 +0530

    Update Collaborators.tsx

--- src/pages/Collaborators.tsx
+++ src/pages/Collaborators.tsx
@@ -135,8 +135,7 @@ const Collaborators = () => {
               <h3 className="text-lg font-semibold text-foreground mb-4">Contact Info</h3>
               <p className="text-muted-foreground">
                Email: info.voltexsolution@gmail.com<br />
-               Phone: <a href="tel:9104629229" className="text-muted-foreground hover: text-primary transition-colors">  9104629229 </a>
-                      <a href="tel:9104629229" className="text-muted-foreground hover: text-primary transition-colors">  9998491595 </a>
+               Phone: 9104629229, 9998491595
                <a href= "https://maps.app.goo.gl/vNQ2SjBrxMMajjhH9">Address: Voltex Solution, shop no 21, Opposite Vinayak Gold palace, Vasant Vihar 1, Ahmedabad </a>
               </p>
             </div>
